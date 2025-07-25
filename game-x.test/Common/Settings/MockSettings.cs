ddc6e02f580b6e067106b504a44b584597d32618
Thuan Dao <daothuan271001@gmail.com>
2025-07-25 18:04:22 +0700

    feat: init test project

---
+++ game-x.test/Common/Settings/MockSettings.cs
@@ -0,0 +1,7 @@
+namespace Test.Common.Settings;
+
+public static class MockSettings
+{
+    // public static readonly IOptions<GameXSettings> Galaxy =
+    //     Options.Create(new GameXSettings { MerchantNumber = "M-12345" });
+}
---
+++ game-x.test/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace game_x.test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
