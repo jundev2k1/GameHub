b8f21edb516e14f680d8ef4d3b09d368501e2f7c
jun <jun@example.com>
2025-07-28 15:21:29 +0700

    refactor: modify to use EmailCacheService for verify email

--- game-x.application/Services/Verification/MemoryVerificationCodeService.cs
+++ game-x.application/Services/Verification/MemoryVerificationCodeService.cs
@@ -1,5 +1,5 @@
-﻿using game_x.application.Contract.Infrastructure.Services.VerificationCode;
-using Microsoft.Extensions.Caching.Memory;
+﻿using game_x.application.Contract.Infrastructure.Caching;
+using game_x.application.Contract.Infrastructure.Services.VerificationCode;
 
 namespace game_x.application.Services.Verification;
 
@@ -9,39 +9,28 @@ public static class VerificationPurposes
     public const string ForgotPassword = "forgot-password";
 }
 
-public sealed class MemoryVerificationCodeService(IMemoryCache cache) : IVerificationCodeService
+public sealed class MemoryVerificationCodeService(IEmailCacheService emailCache) : IVerificationCodeService
 {
     private static readonly TimeSpan DefaultExpiration = TimeSpan.FromMinutes(10);
 
     public string GenerateCode(string email, string purpose, TimeSpan? expiresIn = null)
     {
-        var code = new Random()
+        var randomCode = new Random()
             .Next(0, 100000000)
             .ToString("D8");
-        var cacheKey = GetCacheKey(email, purpose);
-        var options = new MemoryCacheEntryOptions
-        {
-            AbsoluteExpirationRelativeToNow = expiresIn ?? DefaultExpiration
-        };
-
-        cache.Set(cacheKey, code, options);
-        return code;
+        emailCache.SetCode(email, purpose, randomCode, expiresIn ?? DefaultExpiration);
+        return randomCode;
     }
 
     public bool VerifyCode(string email, string purpose, string inputCode)
     {
-        var cacheKey = GetCacheKey(email, purpose);
-        if (cache.TryGetValue(cacheKey, out string? storedCode) && storedCode == inputCode)
+        var storedCode = emailCache.GetCode(email, purpose);
+        if (storedCode == inputCode)
         {
-            cache.Remove(cacheKey);
+            emailCache.RemoveCode(email, purpose);
             return true;
         }
 
         return false;
-    }
-
-    private static string GetCacheKey(string userId, string purpose)
-    {
-        return $"{purpose}:{userId}";
     }
 }
