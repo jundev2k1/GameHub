0b51eb37f71b73a68f0051868eca9409ed5fab44
“khoidang2110” <khoidang2110@gmail.com>
2025-07-30 14:01:02 +0700

    withdrawal

--- game-x.api/Controllers/Client/OrderController.cs
+++ game-x.api/Controllers/Client/OrderController.cs
@@ -1,16 +1,24 @@
 using game_x.application.Features.OrderManagement.Client.Commands.Trade.Buy;
+using game_x.application.Features.OrderManagement.Client.Commands.Trade.Sell;
 
 namespace game_x.api.Controllers.Staff.Order;
 
 [Route("v2/order/tron/usdt")]
-public class OrderController : BaseApiController
+public class OrderV2Controller : BaseApiController
 {
 
     [HttpPost("deposit")]
-    public async Task<IActionResult> CreateBuyOrderAsync(CreateBuyOrderCommand command)
+    public async Task<IActionResult> CreateDepositOrderAsync(CreateBuyOrderCommand command)
     {
         return ApiResponseFactory.Ok(await Mediator.Send(command), MessageCode.System.Accepted);
     }
-
-
+}
+[Route("v1/order/tron/usdt")]
+public class OrderV1Controller : BaseApiController
+{
+    [HttpPost("withdrawal")]
+    public async Task<IActionResult> CreateWithdrawalOrderAsync(CreateSellOrderCommand command)
+    {
+        return ApiResponseFactory.Ok(await Mediator.Send(command), MessageCode.System.Accepted);
+    }
 }
