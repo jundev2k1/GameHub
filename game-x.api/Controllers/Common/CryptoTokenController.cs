abdeb7aae232afc9a1d5cf74d3c2b99002e0e1fc
Thuan Dao <daothuan271001@gmail.com>
2025-08-11 15:11:50 +0700

    feat(CryptoToken): update listing

--- game-x.api/Controllers/Common/CryptoTokenController.cs
+++ game-x.api/Controllers/Common/CryptoTokenController.cs
@@ -1,12 +1,8 @@
-using game_x.api.Common;
-using game_x.application.Common.Filters;
-using game_x.application.Features.ChainTransactions.Client.Queries.GetUsdtLedgerCriteriaByUser;
-using game_x.application.Features.ChainTransactions.Client.Queries.GetUsdtLedgerDetail;
 using game_x.application.Features.ChainTransactions.Shared.Queries.GetCryptoTokenList;
 
 namespace game_x.api.Controllers.Common;
 
-[Authorize(Roles = AppRoles.User)]
+[Authorize(Roles = $"{AppRoles.Root},{AppRoles.Admin},{AppRoles.Cs},{AppRoles.User}")]
 [Route("/api/crypto-tokens")]
 public class CryptoTokenController : BaseApiController
 {
