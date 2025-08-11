e3ac2a72f7dcbba2d288125dfaf32fc89ca5983d
“khoidang2110” <khoidang2110@gmail.com>
2025-08-11 16:53:39 +0700

    validator

---
+++ game-x.application/Features/Games/Commands/GameWallet/Deposit/WalletDepositValiator.cs
@@ -0,0 +1,13 @@
+
+namespace game_x.application.Features.Games.Commands.GameWallet.Deposit;
+
+public sealed class WalletDepositValidator : AbstractValidator<WalletDepositCommand>
+{
+    public WalletDepositValidator()
+    {
+        RuleFor(x => x.Quota)
+            .NotEmpty().WithMessage($"{nameof(WalletDepositCommand.Quota)} is required.")
+            .GreaterThanOrEqualTo(1).WithMessage($"{nameof(WalletDepositCommand.Quota)} must be at least 1.");
+
+    }
+}
---
+++ game-x.application/Features/Games/Commands/GameWallet/Withdrawal/WalletWithdrawalValiator.cs
@@ -0,0 +1,13 @@
+
+namespace game_x.application.Features.Games.Commands.GameWallet.Withdrawal;
+
+public sealed class WalletWithdrawalValidator : AbstractValidator<WalletWithdrawalCommand>
+{
+    public WalletWithdrawalValidator()
+    {
+        RuleFor(x => x.Quota)
+            .NotEmpty().WithMessage($"{nameof(WalletWithdrawalCommand.Quota)} is required.")
+            .GreaterThanOrEqualTo(1).WithMessage($"{nameof(WalletWithdrawalCommand.Quota)} must be at least 1.");
+
+    }
+}
