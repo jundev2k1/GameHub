6af4a6263843501e564789dbe6fa233040637705
jun <jun@example.com>
2025-08-19 20:22:02 +0700

    chores: add env

--- game-x.infrastructure/Security/TokenService.cs
+++ game-x.infrastructure/Security/TokenService.cs
@@ -1,15 +1,17 @@
 ﻿using game_x.application.Common.Abstractions;
 using game_x.application.Contract.Infrastructure.Security;
+using game_x.share.Settings;
+using Microsoft.Extensions.Options;
 using System.Security.Cryptography;
 
 namespace game_x.infrastructure.Security;
 
-public sealed class TokenService : ITokenService, IServices
+public sealed class TokenService(IOptions<RefreshTokenSettings> settings) : ITokenService, IServices
 {
     public RefreshTokenGenerateDto GenerateRefreshToken(string userId)
     {
         var rawToken = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
-        var expiresAt = DateTime.UtcNow.AddDays(7);
+        var expiresAt = DateTime.UtcNow.AddMinutes(settings.Value.DurationInMinutes);
         return new RefreshTokenGenerateDto
         {
             Token = rawToken,
