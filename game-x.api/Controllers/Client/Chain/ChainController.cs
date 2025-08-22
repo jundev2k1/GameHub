fec29550f140e92c17a522fe3d776940bcc2f88d
Thuan Dao <daothuan271001@gmail.com>
2025-08-22 15:24:38 +0700

    chore(ChainTransaction): remove ongoing transaction apis

--- game-x.api/Controllers/Client/Chain/ChainController.cs
+++ game-x.api/Controllers/Client/Chain/ChainController.cs
@@ -1,11 +1,9 @@
 using game_x.api.Common;
 using game_x.application.Common.Filters;
-using game_x.application.Features.ChainTransactions.Admin.Queries.GetTransactionDetailById;
 using game_x.application.Features.ChainTransactions.Client.Commands.TronUsdtWithdrawal;
 using game_x.application.Features.ChainTransactions.Client.Commands.TronUsdtDeposit;
 using game_x.application.Features.ChainTransactions.Client.Queries.GetMyTransactionDetail;
 using game_x.application.Features.ChainTransactions.Client.Queries.GetMyTransactions;
-using game_x.application.Features.ChainTransactions.Client.Queries.GetOngoingTransactionCriteriaByClient;
 
 namespace game_x.api.Controllers.Client.Chain;
 
@@ -45,28 +43,6 @@ public sealed class ChainController : BaseApiController
     public async Task<IActionResult> GetTransactionByIdAsync(Guid transactionId)
     {
         var query = new GetMyTransactionDetailQuery(transactionId);
-        var result = await Mediator.Send(query);
-        return ApiResponseFactory.Ok(result);
-    }
-    
-    [HttpGet("chain-transactions/on-going")]
-    public async Task<IActionResult> GetOngoingTransactionByCriteriaAsync([AsParameters] SearchCriteriaRequest parameters)
-    {
-        var filters = QueryConverter.ToFilters(parameters.Filters, parameters.Keyword);
-        var sorts = QueryConverter.ToSorts(parameters.Sorts);
-        var query = new GetOngoingTransactionCriteriaByClientQuery(
-            filters,
-            sorts,
-            parameters.PageNumber,
-            parameters.PageSize);
-        var result = await Mediator.Send(query);
-        return ApiResponseFactory.Ok(result);
-    }
-
-    [HttpGet("chain-transactions/{transactionId}/on-going")]
-    public async Task<IActionResult> GetOngoingTransactionByIdAsync(Guid transactionId)
-    {
-        var query = new GetTransactionDetailByIdQuery(transactionId);
         var result = await Mediator.Send(query);
         return ApiResponseFactory.Ok(result);
     }
