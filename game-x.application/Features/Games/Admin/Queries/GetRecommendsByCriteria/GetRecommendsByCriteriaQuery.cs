a8d8dd7dadfcac8fe45fd82d81547fc39bda54d8
jun <jun@example.com>
2025-09-08 14:50:02 +0700

    feat(Game Recommend): Add APIs to get detail and get list of game recommend

---
+++ game-x.application/Features/Games/Admin/Queries/GetRecommendsByCriteria/GetRecommendsByCriteriaQuery.cs
@@ -0,0 +1,22 @@
+﻿using game_x.application.Common.Abstractions.Pagination;
+using game_x.application.Common.Filters;
+using game_x.application.Features.Games.Dtos;
+
+namespace game_x.application.Features.Games.Admin.Queries.GetRecommendsByCriteria;
+
+public record GetRecommendsByCriteriaQuery(
+    IEnumerable<QueryFilter> Filters,
+    IEnumerable<QuerySort> Sorts,
+    int PageIndex = 1,
+    int PageSize = 20) : IQuery<PaginationResult<GetRecommendByCriteriaListItem>>;
+
+public record GetRecommendByCriteriaListItem(
+    Guid Id,
+    string Name,
+    string Description,
+    int? BannerId,
+    PublishStatus Status,
+    DateTime? StartDate,
+    DateTime? EndDate,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt);
