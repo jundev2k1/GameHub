7988286d982327fc0f158cabd1d4819adde7b00d
jun <jun@example.com>
2025-09-16 18:04:11 +0700

    feat (Live Stream): Add API for get list of viewers in a stream

--- game-x.api/Controllers/Client/LiveStream/LiveStreamController.cs
+++ game-x.api/Controllers/Client/LiveStream/LiveStreamController.cs
@@ -1,11 +1,21 @@
 ﻿using game_x.application.Features.LiveStreams.Commands.JoinLiveStream;
 using game_x.application.Features.LiveStreams.Commands.PerformAction;
+using game_x.application.Features.LiveStreams.Queries.GetViewersByStream;
 
 namespace game_x.api.Controllers.Client.LiveStream;
 
 [Route("/api/user/livestreams")]
 public sealed class LiveStreamController : BaseApiController
 {
+    [Authorize(Roles = AppRoles.User)]
+    [HttpGet("{streamKey}/viewers")]
+    public async Task<IActionResult> GetViewersByStreamAsync(string streamKey)
+    {
+        var query = new GetViewersByStreamQuery(streamKey);
+        var result = await Mediator.Send(query);
+        return ApiResponseFactory.Ok(result);
+    }
+
     [Authorize(Roles = AppRoles.User)]
     [HttpPost("{id:guid}")]
     public async Task<IActionResult> JoinLiveStreamAsync(Guid id)
@@ -16,7 +26,7 @@ public sealed class LiveStreamController : BaseApiController
     }
 
     [Authorize(Roles = AppRoles.User)]
-    [HttpPost("{streamId}/viewers/{viewerId}/actions")]
+    [HttpPost("{streamId:guid}/viewers/{viewerId}/actions")]
     public async Task<IActionResult> PerformActionAsync(Guid streamId, string viewerId, PerformActionCommand command)
     {
         await Mediator.Send(command with { StreamId = streamId, ViewerId = viewerId });
