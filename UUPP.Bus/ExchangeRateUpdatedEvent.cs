60dd5b1ed24df0eb9f63bc8fa459d38b678e10e5
Thuan Dao <daothuan271001@gmail.com>
2025-09-25 16:40:32 +0700

    chore: remove UUPP project

--- UUPP.Bus/ExchangeRateUpdatedEvent.cs
+++
@@ -1,8 +0,0 @@
-namespace UUPP.Bus;
-
-public class ExchangeRateUpdatedEvent
-{
-    public string CryptoSymbol { get; set; } = String.Empty;
-    public string FiatCode { get; set; } = String.Empty;
-    public decimal SpotPrice { get; set; }
-}
