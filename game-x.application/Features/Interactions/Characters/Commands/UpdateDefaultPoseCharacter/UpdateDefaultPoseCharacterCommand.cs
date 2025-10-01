ab037cafa32fc2e2ad0a102ab19fa1bf769c1d6d
jun <jun@example.com>
2025-10-01 17:17:20 +0700

    feat (Interaction characters): Add API to update default pose

---
+++ game-x.application/Features/Interactions/Characters/Commands/UpdateDefaultPoseCharacter/UpdateDefaultPoseCharacterCommand.cs
@@ -0,0 +1,8 @@
+﻿using game_x.application.Common.Files;
+using System.Text.Json.Serialization;
+
+namespace game_x.application.Features.Interactions.Characters.Commands.UpdateDefaultPoseCharacter;
+
+public record UpdateDefaultPoseCharacterCommand(
+    [property: JsonIgnore] Guid? Id,
+    FileUpload File) : ICommand;
---
+++ game-x.application/Features/Interactions/Characters/Commands/UpdateDefaultPoseCharacter/UpdateDefaultPoseCharacterValidator.cs
@@ -0,0 +1,30 @@
+﻿using game_x.application.Common.Files;
+
+namespace game_x.application.Features.Interactions.Characters.Commands.UpdateDefaultPoseCharacter;
+
+public sealed class UpdateDefaultPoseCharacterValidator : AbstractValidator<UpdateDefaultPoseCharacterCommand>
+{
+    public UpdateDefaultPoseCharacterValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage($"{nameof(UpdateDefaultPoseCharacterCommand.Id)} is required.");
+
+        RuleFor(x => x.File)
+            .Must(BeAValidFileType).WithMessage("Invalid file type.")
+            .Must(BeAValidFileSize).WithMessage("File upload must be not greater than 10 MB.");
+    }
+
+    private bool BeAValidFileType(FileUpload file)
+    {
+        var validExtensions = new[] { ".jpg", ".jpeg", ".png" };
+        var validMimeTypes = new[] { "image/jpeg", "image/png" };
+        return validExtensions.Contains(file.Extension)
+            && validMimeTypes.Contains(file.ContentType);
+    }
+
+    private bool BeAValidFileSize(FileUpload file)
+    {
+        var maxSize = 10 * 1080 * 1080; // 10 MB
+        return file.Length < maxSize;
+    }
+}
