188c7f1d6aeab5d44be6282294b5905de7e2f7a2
jun <jun@example.com>
2025-11-28 13:18:59 +0700

    feat(AppSetting): Add entity and ef config

---
+++ game-x.domain/Entities/AppSetting.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace game_x.domain.Entities;
+
+public sealed class AppSetting : BaseEntity<int>
+{
+    public string Key { get; private set; } = string.Empty;
+    public string Value { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+    public bool IsEditable { get; private set; }
+
+    public static AppSetting Create(string key, string value, string desc, bool isEdit = false)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(key, nameof(key));
+        ArgumentException.ThrowIfNullOrWhiteSpace(key, nameof(value));
+
+        var isExist = typeof(AppSettingConstant).GetFields(BindingFlags.Public | BindingFlags.Static)
+            .Any(f => f.Name.StartsWith("KEY_") && f.Name == key);
+        if (!isExist) throw new ArgumentException(key, nameof(key));
+
+        return new AppSetting
+        {
+            Key = key,
+            Value = value,
+            Description = desc,
+            IsEditable = isEdit,
+        };
+    }
+}
