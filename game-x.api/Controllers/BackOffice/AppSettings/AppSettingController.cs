311a993220933a786ce07109fb3d5660fe6d199a
jun <jun@example.com>
2025-11-28 14:37:29 +0700

    chore: add missing API adapter

---
+++ game-x.api/Controllers/BackOffice/AppSettings/AppSettingController.cs
@@ -0,0 +1,16 @@
+﻿using game_x.application.Features.AppSettings.Commands.UpdateSettings;
+using game_x.application.Features.AppSettings.DTOs;
+
+namespace game_x.api.Controllers.BackOffice.AppSettings;
+
+[Authorize(Roles = AppRoles.Admin)]
+[Route("/api/back-office/app-settings")]
+public sealed class AppSettingController : BaseApiController
+{
+    [HttpPut]
+    public async Task<IActionResult> UpdateSettingsAsync(AppSettingInputDto[] request)
+    {
+        await Mediator.Send(new UpdateSettingsCommand(request));
+        return ApiResponseFactory.NoContent();
+    }
+}
