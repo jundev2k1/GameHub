ac2fd229f11aea5db044d3bf724b32998bfe8ccb
jun <jun@example.com>
2025-11-28 17:25:53 +0700

    fix(UpdateSettingValidator): wrong logic to check range

--- game-x.application/Features/AppSettings/Commands/UpdateSettings/UpdateSettingsValidator.cs
+++ game-x.application/Features/AppSettings/Commands/UpdateSettings/UpdateSettingsValidator.cs
@@ -16,7 +16,7 @@ public sealed class UpdateSettingsValidator : AbstractValidator<UpdateSettingsCo
     private bool HaveValidAppSetting(AppSettingInputDto setting)
     {
         if (setting.Key is AppSettingConstant.KEY_TALENT_COMMISSION_RATE
-            && !decimal.TryParse(setting.Value, out var rate) && (rate < 0 || rate > 100))
+            && (!decimal.TryParse(setting.Value, out var rate) || (rate < 0 || rate > 100)))
             return false;
 
         return true;
