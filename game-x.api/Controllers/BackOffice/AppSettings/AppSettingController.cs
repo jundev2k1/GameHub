c4f1cad968bb88a510dbf55b393597b720af2d6a
jun <jun@example.com>
2025-11-28 14:48:11 +0700

    feat(AppSetting): Add API to get all of app settings

--- game-x.api/Controllers/BackOffice/AppSettings/AppSettingController.cs
+++ game-x.api/Controllers/BackOffice/AppSettings/AppSettingController.cs
@@ -1,5 +1,6 @@
 ﻿using game_x.application.Features.AppSettings.Commands.UpdateSettings;
 using game_x.application.Features.AppSettings.DTOs;
+using game_x.application.Features.AppSettings.Queries.GetAllSettings;
 
 namespace game_x.api.Controllers.BackOffice.AppSettings;
 
@@ -7,6 +8,13 @@ namespace game_x.api.Controllers.BackOffice.AppSettings;
 [Route("/api/back-office/app-settings")]
 public sealed class AppSettingController : BaseApiController
 {
+    [HttpGet]
+    public async Task<IActionResult> GetAllSettingsAsync()
+    {
+        var result = await Mediator.Send(new GetAllSettingsQuery());
+        return ApiResponseFactory.Ok(result);
+    }
+
     [HttpPut]
     public async Task<IActionResult> UpdateSettingsAsync(AppSettingInputDto[] request)
     {
