f52aeb23e003eb9a9d735b62c0eaf09ba8cd12db
jun <jun@example.com>
2025-12-17 17:46:31 +0700

    chore: log job

--- game-x.infrastructure/BackgroundJobs/Jobs/GameSessionTimeoutCheckerJob.cs
+++ game-x.infrastructure/BackgroundJobs/Jobs/GameSessionTimeoutCheckerJob.cs
@@ -1,4 +1,5 @@
-﻿using game_x.application.Contract.Jobs;
+﻿using game_x.application.Contract.Infrastructure.Logger;
+using game_x.application.Contract.Jobs;
 using game_x.application.Contract.Persistence.Repo;
 using game_x.share.Settings;
 using Microsoft.Extensions.Options;
@@ -8,7 +9,8 @@ namespace game_x.infrastructure.BackgroundJobs.Jobs;
 public sealed class GameSessionTimeoutCheckerJob(
     IUnitOfWork unitOfWork,
     IUserGameSessionRepo userGameSessionRepo,
-    IOptions<RecurringJobSettings> jobOptions) : IRecurringJob
+    IOptions<RecurringJobSettings> jobOptions,
+    IAppLogger<GameSessionTimeoutCheckerJob> logger) : IRecurringJob
 {
     public string JobId => "game-session-timeout-checker";
     public string CronExpression => jobOptions.Value.GameSessionTimeoutCheckerJob;
@@ -19,9 +21,11 @@ public sealed class GameSessionTimeoutCheckerJob(
 
     public async Task ExecuteAsync(CancellationToken ct = default)
     {
+        logger.LogInformation("Job is running...");
         await unitOfWork.WithTransactionAsync(async () =>
         {
             await userGameSessionRepo.BulkUpdateExpiredGameSessionsAsync(LimitRangeCount, ct);
         }, ct);
+        logger.LogInformation("Job has stopped...");
     }
 }
