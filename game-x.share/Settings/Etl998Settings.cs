3c689772e0b258ad258bff1109c408db91aa0844
Thuan Dao <daothuan271001@gmail.com>
2025-12-25 10:24:14 +0700

    build: update setting

--- game-x.share/Settings/Etl998Settings.cs
+++ game-x.share/Settings/Etl998Settings.cs
@@ -2,6 +2,7 @@
 
 public sealed class Etl998Settings
 {
+    public string Host { get; set; } = string.Empty;
     public string AgentId { get; set; } = string.Empty;
     public string Md5Key { get; set; } = string.Empty;
     public string DesKey { get; set; } = string.Empty;
