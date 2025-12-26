c2432cbc06f9a11ec8351179ce710db18e98cab7
jun <jun@example.com>
2025-12-26 13:34:53 +0700

    feat(GameSlot): add settings file

---
+++ game-x.share/Settings/GameSlotSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace game_x.share.Settings;
+
+public sealed class GameSlotSettings : BaseSettings
+{
+    public string Host { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+}
