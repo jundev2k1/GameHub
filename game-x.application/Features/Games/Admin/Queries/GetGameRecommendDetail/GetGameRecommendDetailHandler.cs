8a164433fc2edbad8e7b5deaf97ad27208e93014
jun <jun@example.com>
2025-12-30 17:41:07 +0700

    fix(GetRecommendDetail): fix an object not fully covered, causing side effects

--- game-x.application/Features/Games/Admin/Queries/GetGameRecommendDetail/GetGameRecommendDetailHandler.cs
+++ game-x.application/Features/Games/Admin/Queries/GetGameRecommendDetail/GetGameRecommendDetailHandler.cs
@@ -1,15 +1,37 @@
-﻿using game_x.application.Contract.Persistence.Repo;
+﻿using game_x.application.Contract.Infrastructure.Caching;
+using game_x.application.Contract.Persistence.Repo;
 using game_x.application.Features.Games.Dtos;
 
 namespace game_x.application.Features.Games.Admin.Queries.GetGameRecommendDetail;
 
 public sealed class GetGameRecommendDetailHandler(
-    IGameRecommendRepo gameRecommendRepo) : IQueryHandler<GetGameRecommendDetailQuery, GameRecommendDto>
+    IGameProviderCacheService gameProviderCache,
+    IGameRecommendRepo gameRecommendRepo) : IQueryHandler<GetGameRecommendDetailQuery, GetGameRecommendDetailDto>
 {
-    public async Task<GameRecommendDto> Handle(GetGameRecommendDetailQuery request, CancellationToken ct = default)
+    public async Task<GetGameRecommendDetailDto> Handle(GetGameRecommendDetailQuery request, CancellationToken ct = default)
     {
         var targetGame = await gameRecommendRepo.GetAsync(request.Id, ct);
-        var result = targetGame.Adapt<GameRecommendDto>();
+        var dto = targetGame.Adapt<GameRecommendDto>();
+
+        var gameList = gameProviderCache.GameList
+            .ToDictionary(g => g.LocalId, g => g);
+        var tasks = dto.Items.Select(i => MapToListItem(i, gameList));
+        var allItems = await Task.WhenAll(tasks);
+        var items = allItems.Where(i => i != null).ToArray()!;
+        var result = dto.Adapt<GetGameRecommendDetailDto>();
+        result.Items = items!;
+        return result;
+    }
+
+    private async Task<GameRecommendListItemDto?> MapToListItem(GameRecommendItemDto item, Dictionary<int, GameInfoDto> gameList)
+    {
+        if (!gameList.TryGetValue(item.LocalGameId, out var gameInfo)) return null;
+        if (gameInfo.Thumbnail != null)
+        {
+            var thumbnailUrl = await gameProviderCache.GetGameThumbnail(gameInfo);
+            gameInfo.Thumbnail.Url = thumbnailUrl;
+        }
+        var result = new GameRecommendListItemDto(gameInfo, item);
         return result;
     }
 }
--- game-x.application/Features/Games/Admin/Queries/GetGameRecommendDetail/GetGameRecommendDetailQuery.cs
+++ game-x.application/Features/Games/Admin/Queries/GetGameRecommendDetail/GetGameRecommendDetailQuery.cs
@@ -1,5 +1,23 @@
 ﻿using game_x.application.Features.Games.Dtos;
+using System.Text.Json.Serialization;
 
 namespace game_x.application.Features.Games.Admin.Queries.GetGameRecommendDetail;
 
-public record GetGameRecommendDetailQuery(Guid Id) : IQuery<GameRecommendDto>;
+public record GetGameRecommendDetailQuery(Guid Id) : IQuery<GetGameRecommendDetailDto>;
+
+public class GetGameRecommendDetailDto
+{
+    [JsonIgnore]
+    public int LocalId { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    [JsonIgnore]
+    public int? BannerId { get; set; }
+    public PublishStatus Status { get; set; } = PublishStatus.Draft;
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public GameRecommendListItemDto[] Items { get; set; } = [];
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
