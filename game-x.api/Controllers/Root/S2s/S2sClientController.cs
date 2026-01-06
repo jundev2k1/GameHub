7498c6c07048222bcc5bb62e284bbf63b6e799ef
jun <jun@example.com>
2026-01-06 14:54:38 +0700

    feat(S2S): add API to update S2s client

--- game-x.api/Controllers/Root/S2s/S2sClientController.cs
+++ game-x.api/Controllers/Root/S2s/S2sClientController.cs
@@ -1,24 +1,42 @@
 ﻿using game_x.application.Features.S2s.Commands.CreateS2sClient;
+using game_x.application.Features.S2s.Commands.UpdateS2sClient;
 
 namespace game_x.api.Controllers.Root.S2s;
 
 [Authorize(Roles = AppRoles.Root)]
-[Route("api/root/s2s")]
+[Route("api/root/s2s/client")]
 public sealed class S2sClientController : BaseApiController
 {
     /// <summary>
-    /// Creates a new Server-to-Server (S2S) client configuration.
+    /// Creates a new Server-to-Server (S2S) client configuration
     /// </summary>
     /// <remarks>
     /// This API registers a new S2S client used for server-to-server authentication
-    /// and secure communication between external applications and the system.
+    /// and secure communication between external applications and the system
     /// </remarks>
     /// <param name="command">The request payload containing S2S client information</param>
-    /// <returns>Returns HTTP 201 (Created) when the S2S client is successfully created.</returns>
+    /// <returns>Returns HTTP 201 (Created) when the S2S client is successfully created</returns>
     [HttpPost]
     public async Task<IActionResult> CreateS2sClientAsync(CreateS2sClientCommand command)
     {
         await Mediator.Send(command);
         return ApiResponseFactory.Created();
+    }
+
+    /// <summary>
+    /// Updates an existing Server-to-Server (S2S) client configuration
+    /// </summary>
+    /// <remarks>
+    /// This API updates the configuration of an existing S2S client, including
+    /// metadata, permissions, or security-related settings
+    /// </remarks>
+    /// <param name="clientId">The unique identifier of the S2S client to be updated.</param>
+    /// <param name="command">The request payload containing updated S2S client information</param>
+    /// <returns>Returns HTTP 204 (No Content) when the S2S client is successfully updated</returns>
+    [HttpPut("{clientId}")]
+    public async Task<IActionResult> UpdateS2sClientAsync(string clientId, UpdateS2sClientCommand command)
+    {
+        await Mediator.Send(command with { ClientId = clientId });
+        return ApiResponseFactory.NoContent();
     }
 }
---
+++ game-x.application/Features/S2s/Commands/UpdateS2sClient/UpdateS2sClientCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace game_x.application.Features.S2s.Commands.UpdateS2sClient;
+
+public record UpdateS2sClientCommand(
+    [property: JsonIgnore] string ClientId,
+    string ClientName,
+    string ClientCode,
+    string Notes) : ICommand;
