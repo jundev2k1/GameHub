2d13b20168b6b8c3981d3f36be681ba1ef506b5a
jun <jun@example.com>
2026-01-08 18:12:21 +0700

    feat(S2SAuthService): Add Cache service for performance better

---
+++ game-x.application/Contract/Infrastructure/Caching/IS2sAuthCacheService.cs
@@ -0,0 +1,8 @@
+﻿using game_x.application.Features.S2s.DTOs;
+
+namespace game_x.application.Contract.Infrastructure.Caching;
+
+public interface IS2sAuthCacheService
+{
+    Task<S2sCredentialDto> GetAsync(string keyId, AuthMethod method);
+}
