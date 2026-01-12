01f95b1c5d5371e678b86ec0f0cc1b5051d6e2b5
jun <jun@example.com>
2026-01-12 18:07:50 +0700

    chore(Get User Suggestion): add email for suggestion api

--- game-x.application/Features/Accounts/Dtos/UserSummaryInfo.cs
+++ game-x.application/Features/Accounts/Dtos/UserSummaryInfo.cs
@@ -6,6 +6,7 @@ public sealed class UserSummaryInfo
 {
     public string Id { get; set; } = string.Empty;
     public string Nickname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     [JsonIgnore]
     public int? AvatarId { get; set; }
     public string? Avatar { get; set; }
