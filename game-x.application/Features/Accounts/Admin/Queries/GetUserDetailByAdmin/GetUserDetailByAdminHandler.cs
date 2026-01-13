ccc7e76600cc695c00b2db338c0969e88eea4cbc
jun <jun@example.com>
2026-01-13 18:27:22 +0700

    chore(AdminUserDetail): debug

--- game-x.application/Features/Accounts/Admin/Queries/GetUserDetailByAdmin/GetUserDetailByAdminHandler.cs
+++ game-x.application/Features/Accounts/Admin/Queries/GetUserDetailByAdmin/GetUserDetailByAdminHandler.cs
@@ -1,19 +1,29 @@
+using game_x.application.Contract.Infrastructure.Logger;
 using game_x.application.Contract.Persistence.Repo;
 using game_x.application.Features.Accounts.User.Dtos;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace game_x.application.Features.Accounts.Admin.Queries.GetUserDetailByAdmin;
 
 public sealed class GetUserDetailByAdminHandler(
     IUserRepo userRepo,
-    IGamePlatformBalanceRepo platformBalanceRepo) : IQueryHandler<GetUserDetailByAdminQuery, GetUserDetailByAdminResult>
+    IGamePlatformBalanceRepo platformBalanceRepo,
+    IAppLogger<GetUserDetailByAdminHandler> logger) : IQueryHandler<GetUserDetailByAdminQuery, GetUserDetailByAdminResult>
 {
     public async Task<GetUserDetailByAdminResult> Handle(GetUserDetailByAdminQuery request, CancellationToken ct = default)
     {
+        logger.LogInformation("1");
         var userDetail = await userRepo.GetUserDetailAsync(request.UserId, ct);
+        logger.LogInformation("2");
+        logger.LogInformation(JsonSerializer.Serialize(userDetail));
         var externalBalances = await platformBalanceRepo.GetBalancesByUserIdAsync(userDetail.UserId, ct);
-        return userDetail.Adapt<GetUserDetailByAdminResult>() with
+        var result = userDetail.Adapt<GetUserDetailByAdminResult>() with
         {
             ExternalBalances = externalBalances.Adapt<UserWalletExternalItemDto[]>(),
         };
+        logger.LogInformation("3");
+        logger.LogInformation(JsonSerializer.Serialize(result));
+        return result;
     }
 }
