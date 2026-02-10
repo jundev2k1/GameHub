7a82f268d42b64d9a6ab0dc70c42b8fe8fca3f8e
jun <jun@example.com>
2026-02-10 14:20:56 +0700

    chore: missing file

--- game-x.application/Features/Games/Client/Commands/GameWallet/Withdrawal/WalletWithdrawalValiator.cs
+++ game-x.application/Features/Games/Client/Commands/GameWallet/Withdrawal/WalletWithdrawalValiator.cs
@@ -4,10 +4,6 @@ public sealed class WalletWithdrawalValidator : AbstractValidator<WalletWithdraw
 {
     public WalletWithdrawalValidator()
     {
-        RuleFor(x => x.Amount)
-            .NotEmpty().WithMessage($"{nameof(WalletWithdrawalCommand.Amount)} is required.")
-            .GreaterThanOrEqualTo(0).WithMessage($"{nameof(WalletWithdrawalCommand.Amount)} must be greater than zero.");
-
         RuleFor(x => x.CryptoTokenId)
             .NotEmpty().WithMessage($"{nameof(WalletWithdrawalCommand.CryptoTokenId)} is required.");
     }
