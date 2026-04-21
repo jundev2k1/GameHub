06edd586364158b7299635a5980df82e92ec0d0b
Thuan Dao <daothuan271001@gmail.com>
2026-04-21 17:26:37 +0700

    feat(user): add MemberNumber field

--- game-x.application/Features/Accounts/Admin/Queries/GetUserCriteriaByAdmin/GetUserCriteriaByAdminHandler.cs
+++ game-x.application/Features/Accounts/Admin/Queries/GetUserCriteriaByAdmin/GetUserCriteriaByAdminHandler.cs
@@ -1,10 +1,9 @@
 using game_x.application.Common.Abstractions.Pagination;
 using game_x.application.Common.Filters;
 using game_x.application.Contract.Persistence.Repo;
-using game_x.application.Features.Accounts.Admin.Queries.GetUserCriteriaByAdmin;
 using game_x.application.Features.Accounts.Dtos;
 
-namespace game_x.application.Features.AccountManagement.Admin.Queries.GetUserCriteriaByAdmin;
+namespace game_x.application.Features.Accounts.Admin.Queries.GetUserCriteriaByAdmin;
 
 public sealed class GetUserCriteriaByAdminHandler(
     IUserRepo userRepo,
@@ -20,9 +19,10 @@ public sealed class GetUserCriteriaByAdminHandler(
                 request.Filters,
                 request.Sorts,
                 keyword =>
-                    user => (user.Nickname != null && user.Nickname.Contains(keyword))
-                        || (user.UserName != null && user.UserName.Contains(keyword))
-                        || (user.Email != null && user.Email.Contains(keyword))),
+                    user => user.Nickname.Contains(keyword)
+                        || user.UserName.Contains(keyword)
+                        || user.MemberNumber.Contains(keyword)
+                        || user.Email.Contains(keyword)),
             request.PageIndex ?? 1,
             request.PageSize ?? 20,
             ct);
--- game-x.application/Features/Accounts/Dtos/UserDto.cs
+++ game-x.application/Features/Accounts/Dtos/UserDto.cs
@@ -5,6 +5,7 @@ public class UserDto
 {
     public required string Id { get; set; }
     public required string Nickname { get; set; }
+    public required string MemberNumber { get; set; }
     public required string UserName { get; set; }
     public required string Email { get; set; }
     public UserStatus? Status { get; set; }
