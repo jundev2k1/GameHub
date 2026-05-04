fe5a316de1cd0a290d100e6a29c7690c20283193
jun <jun@example.com>
2026-05-04 15:11:18 +0700

    feat(game recommend): map current translation from game info into response

--- game-x.application/Features/Games/Admin/Queries/GetCurrentGameRecommends/GetCurrentGameRecommendsHandler.cs
+++ game-x.application/Features/Games/Admin/Queries/GetCurrentGameRecommends/GetCurrentGameRecommendsHandler.cs
@@ -1,13 +1,16 @@
 ﻿using game_x.application.Contract.Infrastructure.Caching;
+using game_x.application.Contract.Infrastructure.Security;
 using game_x.application.Features.Games.Dtos;
 
 namespace game_x.application.Features.Games.Admin.Queries.GetCurrentGameRecommends;
 
 public sealed class GetCurrentGameRecommendsHandler(
+    IUserAccessor userAccessor,
     IGameProviderCacheService gameProviderCache) : IQueryHandler<GetCurrentGameRecommendsQuery, GameRecommendListItemDto[]>
 {
     public async Task<GameRecommendListItemDto[]> Handle(GetCurrentGameRecommendsQuery request, CancellationToken ct = default)
     {
+        var lang = userAccessor.GetLanguage();
         var dateTime = DateTime.UtcNow;
         var data = gameProviderCache.GameRecommendList
             .FirstOrDefault(r => ((r.StartDate ?? DateTime.MinValue) <= dateTime) && ((r.EndDate ?? DateTime.MaxValue) >= dateTime));
@@ -17,7 +20,7 @@ public sealed class GetCurrentGameRecommendsHandler(
             .ToDictionary(g => g.LocalId, g => g);
         var tasks = data.Items
             .Where(x => x is {IsActive: true, IsGameActive: true})
-            .Select(i => MapToListItem(i, gameList));
+            .Select(i => MapToListItem(i, gameList, lang));
         var items = await Task.WhenAll(tasks);
         return items
             .Where(i => i?.IsActive == true)
@@ -25,13 +28,23 @@ public sealed class GetCurrentGameRecommendsHandler(
             .ToArray()!;
     }
 
-    private async Task<GameRecommendListItemDto?> MapToListItem(GameRecommendItemDto item, Dictionary<int, GameInfoDto> gameList)
+    private async Task<GameRecommendListItemDto?> MapToListItem(GameRecommendItemDto item, Dictionary<int, GameInfoDto> gameList, string lang)
     {
         if (!gameList.TryGetValue(item.LocalGameId, out var gameInfo)) return null;
         if (gameInfo.Thumbnail != null)
         {
             var thumbnailUrl = await gameProviderCache.GetGameThumbnail(gameInfo);
             gameInfo.Thumbnail.Url = thumbnailUrl;
+        }
+        if (gameInfo.GameTranslations.Count > 0)
+        {
+            var targetLang = gameInfo.GameTranslations!.GetValueOrDefault(lang, null);
+            if (targetLang != null)
+            {
+                gameInfo.Name = targetLang.Name;
+                gameInfo.Description = targetLang.Description;
+                gameInfo.Note = targetLang.Notes;
+            }
         }
         var result = new GameRecommendListItemDto(gameInfo, item);
         return result;
