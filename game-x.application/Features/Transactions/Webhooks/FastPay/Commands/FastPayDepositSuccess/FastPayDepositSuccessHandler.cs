4c5d9155c989b61695f37381f3878e30e86ef23c
jun <jun@example.com>
2026-05-05 09:58:26 +0700

    chore(fast pay webhook): debugging

--- game-x.application/Features/Transactions/Webhooks/FastPay/Commands/FastPayDepositSuccess/FastPayDepositSuccessHandler.cs
+++ game-x.application/Features/Transactions/Webhooks/FastPay/Commands/FastPayDepositSuccess/FastPayDepositSuccessHandler.cs
@@ -1,10 +1,13 @@
 ﻿using game_x.application.Contract.Infrastructure.Caching;
+using game_x.application.Contract.Infrastructure.Logger;
 using game_x.application.Contract.Infrastructure.Security;
 using game_x.application.Events.Transactions.OnUxmTransactionCallback;
+using System.Text.Json;
 
 namespace game_x.application.Features.Transactions.Webhooks.FastPay.Commands.FastPayDepositSuccess;
 
 public sealed class FastPayDepositSuccessHandler(
+    IAppLogger<FastPayDepositSuccessHandler> logger,
     IAsymmetricCryptoService asymmetricCryptoService,
     IAsymmetricKeyCacheService asymmetricKeyCacheService,
     IApplicationEventDispatcher eventDispatcher) : ICommandHandler<FastPayDepositSuccessCommand>
@@ -13,6 +16,9 @@ public sealed class FastPayDepositSuccessHandler(
     {
         var (requestData, signature) = request;
 
+        logger.LogInformation($"Fast Pay Key: {asymmetricKeyCacheService.FastPayPublicKey}");
+        logger.LogInformation($"Signature: {request.Signature}");
+        logger.LogInformation($"Payload: {JsonSerializer.Serialize(request.Data)}");
         // Verify UXM signature
         var isValid = asymmetricCryptoService.VerifySignature(
             asymmetricKeyCacheService.FastPayPublicKey,
