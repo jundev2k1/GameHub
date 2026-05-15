2f2605555432761381300f7379d8aa80acd4b0cb
jun <jun@example.com>
2026-05-15 13:43:15 +0700

    feat(gameplatform translation): add schema

---
+++ game-x.persistence/Migrations/20260515064234_Add_GamePlatformTranslation_Schema.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace game_x.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_GamePlatformTranslation_Schema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "game_platform_translations",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    platform_id = table.Column<int>(type: "integer", nullable: false),
+                    language_code = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    description = table.Column<string>(type: "character varying(4000)", maxLength: 4000, nullable: false, defaultValue: ""),
+                    note = table.Column<string>(type: "character varying(4000)", maxLength: 4000, nullable: false, defaultValue: ""),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_game_platform_translations", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_game_platform_translations_game_platforms_platform_id",
+                        column: x => x.platform_id,
+                        principalTable: "game_platforms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_game_platform_translations_platform_id_language_code",
+                table: "game_platform_translations",
+                columns: new[] { "platform_id", "language_code" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "game_platform_translations");
+        }
+    }
+}
