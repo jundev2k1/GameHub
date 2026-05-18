4c1a95b410743cc775a589a7948dfabec72b771f
jun <jun@example.com>
2026-05-18 20:41:10 +0700

    feat(navigation items): add API to delete navigation item for admin site

--- game-x.api/Controllers/BackOffice/Navigations/NavigationController.cs
+++ game-x.api/Controllers/BackOffice/Navigations/NavigationController.cs
@@ -1,4 +1,5 @@
-﻿using game_x.application.Features.NavigationItems.Admin.Queries.GetAllNavigationItems;
+﻿using game_x.application.Features.NavigationItems.Admin.Commands.DeleteNavigationItem;
+using game_x.application.Features.NavigationItems.Admin.Queries.GetAllNavigationItems;
 
 namespace game_x.api.Controllers.BackOffice.Navigations;
 
@@ -11,5 +12,14 @@ public sealed class NavigationController : BaseApiController
     {
         var result = await Mediator.Send(new GetAllNavigationItemsQuery(), ct);
         return ApiResponseFactory.Ok(result);
+    }
+
+    [Authorize(Roles = AppRoles.Admin)]
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteItemAsync(Guid id, CancellationToken ct = default)
+    {
+        var command = new DeleteNavigationItemCommand(id);
+        await Mediator.Send(command, ct);
+        return ApiResponseFactory.NoContent();
     }
 }
