bba33baca7db12611418203a63a2a8268af67516
jun <jun@example.com>
2026-05-18 19:55:30 +0700

    feat(navigation item): add repository

---
+++ game-x.application/Contract/Persistence/Repo/INavigationItemRepo.cs
@@ -0,0 +1,16 @@
+﻿namespace game_x.application.Contract.Persistence.Repo;
+
+public interface INavigationItemRepo
+{
+    Task<NavigationItem[]> GetAllAsync(CancellationToken ct = default);
+
+    Task CreateAsync(NavigationItem item, CancellationToken ct = default);
+
+    Task UpdateAsync(
+        Guid id,
+        Action<NavigationItem> updateAction,
+        Func<IQueryable<NavigationItem>, IQueryable<NavigationItem>>? preUpdateAction = null,
+        CancellationToken ct = default);
+
+    Task DeleteAsync(Guid id, CancellationToken ct = default);
+}
---
+++ game-x.persistence/Repo/NavigationItemRepo.cs
@@ -0,0 +1,49 @@
+﻿using game_x.application.Common.Abstractions;
+using game_x.application.Contract.Persistence.Repo;
+using game_x.application.Exceptions;
+
+namespace game_x.persistence.Repo;
+
+public sealed class NavigationItemRepo(GameXContext dbContext) : INavigationItemRepo, IRepository
+{
+    public async Task<NavigationItem[]> GetAllAsync(CancellationToken ct = default)
+    {
+        return await dbContext.NavigationItems
+            .AsNoTracking()
+            .AsSplitQuery()
+            .Include(i => i.Translations)
+            .ToArrayAsync(ct);
+    }
+
+    public async Task CreateAsync(NavigationItem item, CancellationToken ct = default)
+    {
+        await dbContext.NavigationItems.AddAsync(item, ct);
+    }
+
+    public async Task UpdateAsync(
+        Guid id,
+        Action<NavigationItem> updateAction,
+        Func<IQueryable<NavigationItem>, IQueryable<NavigationItem>>? preUpdateAction = null,
+        CancellationToken ct = default)
+    {
+        var query = dbContext.NavigationItems
+            .AsQueryable();
+
+        if (preUpdateAction != null)
+            query = preUpdateAction(query);
+
+        var target = await query.FirstOrDefaultAsync(i => i.PublicId == id, ct)
+            ?? throw new NotFoundException(nameof(id), id);
+
+        updateAction?.Invoke(target);
+    }
+
+    public async Task DeleteAsync(Guid id, CancellationToken ct = default)
+    {
+        var target = await dbContext.NavigationItems
+            .FirstOrDefaultAsync(i => i.PublicId == id, ct)
+            ?? throw new NotFoundException(nameof(id), id);
+
+        dbContext.Remove(target);
+    }
+}
