68c19dee8aa63057404e659d055ea438b2353ef0
Thuan Dao <daothuan271001@gmail.com>
2026-05-27 15:40:33 +0700

    fix(mission): mission not found

---
+++ game-x.application/Features/Rewards/Queries/Missions/GetDetailForUser/Query.cs
@@ -0,0 +1,5 @@
+﻿using game_x.application.Features.Rewards.Dtos;
+
+namespace game_x.application.Features.Rewards.Queries.Missions.GetDetailForUser;
+
+public sealed record GetMissionDetailForUserQuery(Guid Id) : IQuery<MissionUserDto>;
---
+++ game-x.application/Features/Rewards/Queries/Missions/GetListForUser/Query.cs
@@ -0,0 +1,6 @@
+﻿using game_x.application.Features.Rewards.Dtos;
+using game_x.domain.Enum.Rewards;
+
+namespace game_x.application.Features.Rewards.Queries.Missions.GetListForUser;
+
+public sealed record GetMissionListForUserQuery(MissionType? Type) : IQuery<IReadOnlyList<MissionUserDto>>;
